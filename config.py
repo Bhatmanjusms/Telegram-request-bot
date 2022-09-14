7a27d910c1088a541b79a6da03476fd27d195e41
Bhatmanjusms <86521081+Bhatmanjusms@users.noreply.github.com>
2022-09-14 10:18:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 #Contains configuration details
 
-master_id = [123456789, 987654321]
-group_id = -1001221917073
-master_username = 'Sangwan5688'
-PROJECT_NAME = "BlackHole"
-PROJECT_URL = "https://github.com/Sangwan5688/BlackHole"
+master_id = [2083503061]
+group_id = -1001665439555
+master_username = 'jack_sparow119'
+PROJECT_NAME = "Telegram-request-bot"
+PROJECT_URL = "https://github.com/Bhatmanjusms/Telegram-request-bot"
 FAQ_URL = "https://github.com/Sangwan5688/BlackHole/wiki/FAQs"
-BOT_NAME = "BlackHole: Request Bot"
-BOT_USERNAME = "BlackHoleRequestbot"
-TOKEN = "yourToken"
+BOT_NAME = "request bot"
+BOT_USERNAME = "ff_request_bot"
+TOKEN = "5626129676:AAEpsV_7V05fTldBSR5ivz7kZ3wfwByhX9c"
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
